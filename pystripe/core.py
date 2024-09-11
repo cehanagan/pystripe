f73993d61baa18c452592c7fa664283b50284ec8
cehanagan <74363782+cehanagan@users.noreply.github.com>
2024-09-11 15:03:24 -0600

    Update core.py

--- pystripe/core.py
+++ pystripe/core.py
@@ -432,7 +432,7 @@ def filter_streaks(img, sigma, level=0, wavelet='db3', crossover=10, threshold=-
         except ValueError:
             threshold = 1
 
-    img = np.array(img, dtype=np.float)
+    img = np.array(img, dtype=np.float64)
     #
     # Need to pad image to multiple of 2
     #
